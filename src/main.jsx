7068857d59afad43e8635bdfd0970ce29b73b2ba
Kunal8990 <gokut2896@gmail.com>
2026-09-16 15:51:42 +0530

    Implement true stroke-by-stroke signature reveal via SVG mask animation

--- src/main.jsx
+++ src/main.jsx
@@ -427,7 +427,7 @@ function AnimatedSignature() {
     setSigKey((k) => k + 1);
     setTimeout(() => {
       setIsSigning(false);
-    }, 4200);
+    }, 6200);
   };
 
   return (
@@ -444,7 +444,7 @@ function AnimatedSignature() {
             type="button"
             className="sig-replay-btn"
             onClick={handleReplay}
-            title="Replay smooth handwritten signature animation"
+            title="Replay handwritten signature animation"
             aria-label="Replay signature animation"
           >
             <RotateCcw size={12} className={isSigning ? "spinning" : ""} />
@@ -462,13 +462,60 @@ function AnimatedSignature() {
             tabIndex={0}
             onKeyDown={(e) => { if (e.key === "Enter" || e.key === " ") handleReplay(e); }}
           >
-            {isSigning && <div className="sig-spark-tracer" />}
-            <img
-              src={signatureImg}
-              alt="Kunal Jha Signature"
-              className="kunal-signature-img"
-              draggable={false}
-            />
+            {/* SVG mask reveals the actual signature image stroke-by-stroke, like real signing */}
+            <svg
+              key={`sig-${sigKey}`}
+              viewBox="0 0 1520 552"
+              className="signature-svg-reveal"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlnsXlink="http://www.w3.org/1999/xlink"
+              aria-label="Kunal Jha Signature"
+            >
+              <defs>
+                <mask id={`sigMask-${sigKey}`} maskUnits="userSpaceOnUse" x="-20" y="-20" width="1560" height="592">
+                  <rect x="-20" y="-20" width="1560" height="592" fill="black" />
+                  {/* Mask strokes animate: white = revealed, black = hidden */}
+                  {/* 1. K stem slash */}
+                  <path
+                    className={`mask-stroke stroke-k-slash ${isSigning ? "is-animating" : "is-done"}`}
+                    d="M 47,498 C 100,390 200,280 304,143"
+                  />
+                  {/* 2. K arms upper + lower */}
+                  <path
+                    className={`mask-stroke stroke-k-arms ${isSigning ? "is-animating" : "is-done"}`}
+                    d="M 230,270 C 275,220 310,185 390,160 M 230,275 C 270,330 300,390 320,445 C 340,475 375,420 395,390"
+                  />
+                  {/* 3. unal cursive flow */}
+                  <path
+                    className={`mask-stroke stroke-unal ${isSigning ? "is-animating" : "is-done"}`}
+                    d="M 395,390 C 420,355 445,335 460,365 C 475,395 490,395 505,365 C 520,340 540,340 555,370 C 568,395 585,395 598,368 C 612,340 630,340 645,368 C 660,395 680,395 695,365 C 710,335 735,215 750,180 C 762,162 772,180 762,225 C 745,305 752,390 785,390"
+                  />
+                  {/* 4. J grand descending loop */}
+                  <path
+                    className={`mask-stroke stroke-j ${isSigning ? "is-animating" : "is-done"}`}
+                    d="M 825,200 C 875,145 920,100 949,102 C 968,112 958,155 935,235 C 900,345 848,468 780,532 C 748,562 728,528 748,475 C 768,422 828,368 920,345 C 958,328 990,342 1010,350"
+                  />
+                  {/* 5. ha */}
+                  <path
+                    className={`mask-stroke stroke-ha ${isSigning ? "is-animating" : "is-done"}`}
+                    d="M 1010,350 C 1038,255 1052,145 1060,90 C 1068,58 1082,68 1078,112 C 1068,185 1062,305 1068,395 C 1080,355 1108,332 1134,332 C 1158,332 1168,368 1182,392 C 1198,368 1218,355 1240,368 C 1260,378 1265,398 1285,398 C 1315,398 1370,375 1440,355"
+                  />
+                  {/* 6. Flourish underline sweep */}
+                  <path
+                    className={`mask-stroke stroke-flourish ${isSigning ? "is-animating" : "is-done"}`}
+                    d="M 260,465 C 460,432 740,408 1045,392 C 1200,386 1362,376 1468,334"
+                  />
+                </mask>
+              </defs>
+              {/* The actual signature image, revealed through the animated mask */}
+              <image
+                href={signatureImg}
+                x="0" y="0"
+                width="1520" height="552"
+                mask={`url(#sigMask-${sigKey})`}
+                preserveAspectRatio="xMidYMid meet"
+              />
+            </svg>
 
             <div className={`sig-motto ${isSigning ? "is-signing" : "is-settled"}`}>
               <span>BUILD</span>
